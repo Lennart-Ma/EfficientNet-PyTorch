c90967fcd75de45b1b464cba6ebe5bbe578ff2df
Lennart <LennartMa.19@gmail.com>
2021-10-28 13:26:16 +0200

    get_mean_std() calcs by default for float values

--- utils/calc_mean_std.py
+++ utils/calc_mean_std.py
@@ -2,9 +2,18 @@ from PIL import Image
 import numpy as np
 import os
 import glob
+from skimage.util import img_as_float
 
 
-def get_mean_std(path):
+
+def get_mean_std(path, img_type=None):
+
+    """
+    Calculates mean and std of images stored in a path - default type is float (https://scikit-image.org/docs/dev/user_guide/data_types.html)
+
+    path: path to images
+    img_type: type of image - default=None means it calculates mean and std for float img
+    """
 
     mean_sum = 0
     std_sum = 0
@@ -13,8 +22,14 @@ def get_mean_std(path):
     for i, image in enumerate(glob.glob(f"{path}/*")):
 
         image = Image.open(image).convert('L')
-        data = np.asarray(image)
         
+        if img_type == "uint8":
+            data = np.asarray(image)
+            print("inside")
+        else:
+            data = np.asarray(image)
+            data = img_as_float(data)
+
         mean = np.mean(data)
         std = np.std(data)
 
@@ -32,5 +47,7 @@ def get_mean_std(path):
 if __name__ == "__main__":
 
     path = r"G:\My Drive\Projektarbeit_ResearchProject\datasets\BUS_classification\resized\train_val\256\500\images"
+
+    #img_type="uint8"
 
     get_mean_std(path)
