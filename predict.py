7be48659f50dc9f57e343802e1c5977c492a9b55
Lennart <LennartMa.19@gmail.com>
2021-11-04 01:11:56 +0100

    Add .csv file writer to save results directly

--- predict.py
+++ predict.py
@@ -5,6 +5,8 @@ import os
 import pandas as pd
 import albumentations
 import argparse
+import glob
+import csv
 
 from efficientnet_pytorch import EfficientNet
 from utils.data import ClassificationDataset
@@ -61,7 +63,7 @@ def predict(model, device, test_loader, ensemble):
     print()
     print()
 
-    return classification_report(targets, predictions), accuracy, auc
+    return classification_report(targets, predictions, output_dict=True), accuracy, auc
 
 
 def calc_ensemble_prediction(collected_predictions):
@@ -102,8 +104,13 @@ if __name__ == "__main__" :
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--output_path", type=str, help="path to the folder where the results (.csv) should be saved")
     parser.add_argument("--dataset", type=str, help="path to the folder containing the images size: 260x260")
     parser.add_argument("--gt", type=str, help="path to the file that contains the gt csv file - see examples under /examples (needs to be added)")
+    parser.add_argument("--perc_data", type=int, help="How much data is used for classification in percentage (Used to name the output csv file uniquely")
+    parser.add_argument("--fake", type=str, help="Use to name the output csv file - if fake data is used type 'fake' else 'no_fake'")
+
+
     parser.add_argument("--single_model_path", default=None, type=str, help="Use only for single model prediction: path to the folder containing the model file: .bin")
     parser.add_argument("--ensemble", type=bool, default=False, help="set to True for ensemble prediction")
     parser.add_argument("--Model0", type=str, default=None, help="Use only for ensemble model prediction: path to the folder containing the 1st model file for ensemble: .bin")
@@ -171,11 +178,45 @@ if __name__ == "__main__" :
 
         auc = roc_auc_score(targets, ensemble_pred)
 
+        classification_report_ensemble = classification_report(targets, ensemble_pred, output_dict=True)
+
         print("Ensemble results: ")
-        print(classification_report(targets, ensemble_pred))
+        print(classification_report_ensemble)
         print("Accuracy: ", accuracy)
         print("AUC: ", auc)
 
+        file_exist=False
+        perc_data = opt.perc_data
+        fake = opt.fake
+
+        for file in glob.glob(os.path.join(opt.output_path, '*.{}'.format("csv"))):
+            file_exist = True
+            with open(file, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(["Ensemble results: "])
+                writer.writerow(["label 1"])
+                writer.writerow([classification_report_ensemble[list(classification_report_ensemble.keys())[0]]])
+                writer.writerow(["label 2"])
+                writer.writerow([classification_report_ensemble[list(classification_report_ensemble.keys())[1]]])
+                writer.writerow(["Accuracy: ", accuracy])
+                writer.writerow(["AUC: ", auc])
+                writer.writerow([""])
+        
+        if not file_exist:
+            with open(os.path.join(opt.output_path, f"Prediction_results_on_test_data_IVUS{perc_data}+{fake}.csv"), 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([f"PREDICTION RESULTS ON TEST DATA WITH EfficientNetb2 model trained on IVUS data {perc_data}+{fake}"])
+                writer.writerow([""])
+                writer.writerow(["Ensemble results: "])
+                writer.writerow(["label 1"])
+                writer.writerow([classification_report_ensemble[list(classification_report_ensemble.keys())[0]]])
+                writer.writerow(["label 2"])
+                writer.writerow([classification_report_ensemble[list(classification_report_ensemble.keys())[1]]])
+                writer.writerow(["Accuracy: ", accuracy])
+                writer.writerow(["AUC: ", auc])
+                writer.writerow([""])
 
     elif opt.single_model_path is not None:
         
@@ -183,11 +224,44 @@ if __name__ == "__main__" :
         model.load_state_dict(torch.load(opt.single_model_path))
         model.to(opt.device)
 
-        report, accuracy, auc = predict(model, opt.device, test_loader, ensemble=False)
+        single_report, accuracy, auc = predict(model, opt.device, test_loader, ensemble=False)
 
-        print("Single Model results: ", report)
+        print("Single Model results: ", single_report)
         print("Accuracy: ", accuracy)
         print("AUC: ", auc)
+
+        file_exist=False
+        perc_data = opt.perc_data
+        fake = opt.fake
+
+        for file in glob.glob(os.path.join(opt.output_path, '*.{}'.format("csv"))):
+            file_exist = True
+            with open(file, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(["Results for : ", os.path.basename(opt.single_model_path)])
+                writer.writerow(["label 1"])
+                writer.writerow([single_report[list(single_report.keys())[0]]])
+                writer.writerow(["label 2"])
+                writer.writerow([single_report[list(single_report.keys())[1]]])
+                writer.writerow(["Accuracy: ", accuracy])
+                writer.writerow(["AUC: ", auc])
+                writer.writerow([""])
+        
+        if not file_exist:
+            with open(os.path.join(opt.output_path, f"Prediction_results_on_test_data_IVUS{perc_data}+{fake}.csv"), 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([f"PREDICTION RESULTS ON TEST DATA WITH EfficientNetb2 model trained on IVUS data {perc_data}+{fake}"])
+                writer.writerow([""])
+                writer.writerow(["Results for : ", os.path.basename(opt.single_model_path)])
+                writer.writerow(["label 1"])
+                writer.writerow([single_report[list(single_report.keys())[0]]])
+                writer.writerow(["label 2"])
+                writer.writerow([single_report[list(single_report.keys())[1]]])
+                writer.writerow(["Accuracy: ", accuracy])
+                writer.writerow(["AUC: ", auc])
+                writer.writerow([""])
 
     else:
         print("No model path specified - read opt.arguments")
